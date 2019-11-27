40ba7b196f30839caa47dbbbd59b647f7e9a9bce
Ruari Day <ruariday@gmail.com>
2019-11-27 16:15:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 
 def square_array(numbers)
   counter = 0 
-  new_array = Array.new()
+  new_array = Array.new(numbers.length)
   while counter < numbers.length do
     new_array[counter] = numbers[counter] **2
     counter += 1
